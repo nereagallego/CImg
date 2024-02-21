3c6578ce9db1c364d92182952e9fb567442c0262
cborjamoreno <cesarborjamoreno@gmail.com>
2024-02-21 16:38:41 +0100

    kubuntu killer

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -40,6 +40,15 @@ demosaicNN = demosaicNearestNeighbor(image);
 
 % 3.2) Demosaicing with bilinear interpolation
 demosaicBI = demosaicingBilinearInterpolation(image);
+% figure; imshow(demosaicBI);
+
+% 4) White balancing
+% 4.1) White balancing using the white world assumption
+% whiteBalanced = whiteBalancingWhiteWorld(demosaicBI);
+% whiteBalanced = whiteBalancingGrayWorld(demosaicBI);
+whiteBalanced = whiteBalancedManualBalancing(demosaicBI);
+figure; imshow(whiteBalanced);
+
 
 function [outputImage] = demosaicNearestNeighbor(inputImage)
     % Get the size of the input image
@@ -141,8 +150,82 @@ function [outputImage] = demosaicingBilinearInterpolation(inputImage)
     if mod(height,2) == 1 && mod(width,2) == 1
         outputImage(end,end,1) = outputImage(end-1,end-1,1);
     end
-
-    figure; imshow(outputImage(:,:,2));
 end
 
+function [outputImage] = whiteBalancingWhiteWorld(inputImage)
+
+    % Initialize the output image
+    outputImage = inputImage;
+
+    % Compute per-channel maximum
+    maxR = max(max(inputImage(:,:,1)));
+    maxG = max(max(inputImage(:,:,2)));
+    maxB = max(max(inputImage(:,:,3)));
+
+    % Normalize each channel by its maximum
+    outputImage(:,:,1) = outputImage(:,:,1) / maxR;
+    outputImage(:,:,2) = outputImage(:,:,2) / maxG;
+    outputImage(:,:,3) = outputImage(:,:,3) / maxB;
+
+    % Normalize by green channel maximum
+    outputImage(:,:,1) = outputImage(:,:,1) * maxG;
+    outputImage(:,:,2) = outputImage(:,:,2) * maxG;
+    outputImage(:,:,3) = outputImage(:,:,3) * maxG;
+    
+    
+   
+end
+
+function [outputImage] = whiteBalancingGrayWorld(inputImage)
+
+    % Initialize the output image
+    outputImage = inputImage;
+
+    % Compute per-channel average
+    avgR = mean(mean(inputImage(:,:,1)));
+    avgG = mean(mean(inputImage(:,:,2)));
+    avgB = mean(mean(inputImage(:,:,3)));
+
+    % Normalize each channel by its average
+    outputImage(:,:,1) = outputImage(:,:,1) / avgR;
+    outputImage(:,:,2) = outputImage(:,:,2) / avgG;
+    outputImage(:,:,3) = outputImage(:,:,3) / avgB;
+
+    % Normalize by green channel average
+    outputImage(:,:,1) = outputImage(:,:,1) * avgG;
+    outputImage(:,:,3) = outputImage(:,:,3) * avgG;
+    outputImage(:,:,2) = outputImage(:,:,2) * avgG;
+    
+   
+end
+
+function [output] = whiteBalancedManualBalancing(inputImage)
+    output = inputImage;
+
+    % Choose manually the gray point
+    % Display the image
+    imshow(inputImage);
+
+    % Get a pixel coordinate by mouse click
+    [x, y] = ginput(1);
+
+    % Round the coordinates as pixel indices must be integers
+    x = round(x);
+    y = round(y);
+
+    % Get the RGB values of the selected pixel
+    pixelValue = inputImage(y, x, :);
+
+    % Deduce the weight of each channel 
+    weightR = 1 / pixelValue(1);
+    weightG = 1 / pixelValue(2);
+    weightB = 1 / pixelValue(3);
+
+    % If the object is recoded as rw, gw, bw use weights for each channel such that kr = k/rw, kg = k/gw, kb = k/bw where k controls the exposure
+    % Normalize each channel by its weight
+    output(:,:,1) = output(:,:,1) * weightR;
+    output(:,:,2) = output(:,:,2) * weightG;
+    output(:,:,3) = output(:,:,3) * weightB;
+
+end
 
