f268e631153e583d75f63179c7b2ff6eea349a75
cborjamoreno <cesarborjamoreno@gmail.com>
2024-03-27 10:43:27 +0100

    color images

--- lab2/deblurring_demo.m
+++ lab2/deblurring_demo.m
@@ -1,75 +1,57 @@
 % Read data
-aperture = imread('apertures/raskar.bmp');
+aperture = imread('apertures/zhou.bmp');
 image = imread('images/penguins.jpg');
-image = image(:, :, 1);
 
 % Noise level (Gaussian noise)
 sigma = 0.005;
 
 % Blur size
-% blurSize = 7; %7
+blurSize = 7; %7
 
-for blurSize = [2, 7, 12]
+% for blurSize = [2, 7, 12]
 
-    f0 = im2double(image);
-    [height, width, channel] = size(f0);
+f0 = im2double(image);
+[height, width, channel] = size(f0);
 
-    % Prior matrix: 1/f law
-    A_star = eMakePrior(height, width) + 0.00000001;
-    C = sigma.^2 * height * width ./ A_star;
+% Prior matrix: 1/f law
+A_star = eMakePrior(height, width) + 0.00000001;
+C = sigma.^2 * height * width ./ A_star;
 
-    % Normalization
-    temp = fspecial('disk', blurSize);
-    flow = max(temp(:));
+% Normalization
+temp = fspecial('disk', blurSize);
+flow = max(temp(:));
 
-    % Calculate effective PSF
-    k1 = im2double(...
-        imresize(aperture, [2*blurSize + 1, 2*blurSize + 1], 'nearest')...
-    );
+% Calculate effective PSF
+k1 = im2double(...
+    imresize(aperture, [2*blurSize + 1, 2*blurSize + 1], 'nearest')...
+);
 
-    k1 = k1 * (flow / max(k1(:)));
+k1 = k1 * (flow / max(k1(:)));
 
-    for sigma = [0.5, 0.05, 0.005]
-        for it = [1, 10, 100]
-            dirName = ['figures/raskar/blur/wiener_pior/sigma_', num2str(sigma)];
-            if ~exist(dirName, 'dir')
-            mkdir(dirName);
-            end
-            filename_rec = [dirName, '/recovered_', num2str(blurSize), '.png'];
-            filename_def = [dirName, '/defocused_', num2str(blurSize), '.png'];
-            % Apply blur
-            f1 = zDefocused(f0, k1, sigma, 0);
-        
-
-            % Recover
-            f0_hat = zDeconvWNR(f1, k1, C);
-            % f0_hat = deconvlucy(f1,k1, it);
-            % f0_hat = deconvwnr(f1,k1, C);
-            imwrite(f0_hat, filename_rec);
-            imwrite(f1, filename_def);
-            
+for sigma = [0.5, 0.05, 0.005]
+    for it = [1, 10, 100]
+        dirName = ['figures/zhou/blur/deconvlucy/color/sigma_', num2str(sigma)];
+        if ~exist(dirName, 'dir')
+        mkdir(dirName);
         end
+        filename_rec = [dirName, '/recovered_', num2str(it), '.png'];
+        filename_def = [dirName, '/defocused_', num2str(it), '.png'];
+
+        % Initialize color image matrices
+        f1 = zeros(height, width, channel);
+        f0_hat = zeros(height, width, channel);
+        
+        % Apply blur and recover for each color channel
+        for ch = 1:channel
+            f0_ch = f0(:,:,ch);
+            f1_ch = zDefocused(f0_ch, k1, sigma, 0);
+            f0_hat_ch = deconvlucy(f1_ch,k1, it);
+            f1(:,:,ch) = f1_ch;
+            f0_hat(:,:,ch) = f0_hat_ch;
+        end
+        
+        imwrite(f0_hat, filename_rec);
+        imwrite(f1, filename_def);
+        
     end
 end
-
-% Apply blur
-f1 = zDefocused(f0, k1, sigma, 0);
-
-% Recover
-f0_hat = zDeconvWNR(f1, k1, C);
-% f0_hat = deconvlucy(f1,k1);
-
-% Display results
-figure;
-
-subplot_tight(1, 3, 1, 0.0, false)
-imshow(f0);
-%title('Focused');
-
-subplot_tight(1, 3, 2, 0.0, false)
-imshow(f1);
-%title('Defocused');
-
-subplot_tight(1, 3, 3, 0.0, false)
-imshow(f0_hat);
-%title('Recovered');
