74efcd14cd053a469153759cc890304120483f48
Nerea <801950@unizar.es>
2024-04-23 23:00:14 +0200

    results local tone mapping

--- lab3/Part3/part3.m
+++ lab3/Part3/part3.m
@@ -1,9 +1,9 @@
 %load HDR
-hdr = hdrread('../Part1/Results/hdr_image.hdr');
+hdr = hdrread('../Part1/Results/tent_weights/hdr_image.hdr');
 
 fignum = 6;
 
-for dR = [2, 4, 6]
+for dR = [2, 3, 4, 5, 6, 7, 8]
 
     imwrite( ...
         durand_tonemapping(hdr, dR, fignum), ...
