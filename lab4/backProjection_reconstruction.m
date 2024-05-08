f42c72d90b46ade48ed9458a8b582be1cf475749
Nerea <801950@unizar.es>
2024-05-08 16:59:00 +0200

    Refactor backProjection_reconstruction function for improved readability and maintainability @cborjamoreno

--- lab4/backProjection_reconstruction.m
+++ lab4/backProjection_reconstruction.m
@@ -1,3 +1,47 @@
-function G = backProjection_reconstruction(x_l, x_s, t_v)
+function G = backProjection_reconstruction(data)
+    laser_origin = data.laserOrigin;
+    spad_origin = data.spadOrigin;
+
+    x_l = data.laserPositions;
+    x_s = data.spadPositions;
+
+    v_c = data.volumePosition;
+    v_s = data.volumeSize;
+
+    resolution = 16;
+
+    G = zeros(resolution, resolution, resolution);
+
+    for v_i = 1:resolution
+        for v_j = 1:resolution
+            for v_k = 1:resolution
+                center = v_c - 0.5 * [v_s; v_s; v_s] + [v_i; v_j; v_k] * v_s / resolution;
+                for l_i = 1:size(x_l, 1)
+                    for l_j = 1:size(x_l, 2)
+                        for s_i = 1:size(x_s, 1)
+                            for s_j = 1:size(x_s, 2)
+                                l = x_l(l_i, l_j, :);
+                                s = x_s(s_i, s_j, :);
+                                l = l(:);
+                                s = s(:);
+                                d1 = norm(laser_origin(:) - l);
+                                d2 = norm(spad_origin(:) - s);
+                                d3 = norm(l - center(:));
+                                d4 = norm(s - center(:));
+
+                                t = (d1 + d2 + d3 + d4);
+
+                                index = round(t / data.deltaT) + data.t0;
+
+                                G(v_i, v_j, v_k) = G(v_i, v_j, v_k) + data.data(l_i, l_j, s_i, s_j, index);
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    
     
 end
--- lab4/lab4.m
+++ lab4/lab4.m
@@ -1,5 +1,8 @@
 % NLOS Imaging
 
 % Load the data
-data = load_hdf5_dataset('Data/planes_d=0.5_l=[16x16]_s=[16x16].hdf5');
+% data = load_hdf5_dataset('Data\Z_d=0.5_l=[1x1]_s=[256x256].hdf5');
 
+G = backProjection_reconstruction(data);
+
+volshow(G, volshow_config)
