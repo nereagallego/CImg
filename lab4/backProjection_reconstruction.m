c9f97888d52f522a5d805d27b6f0fd8b110b6337
Nerea <801950@unizar.es>
2024-05-14 00:09:55 +0200

    fixed distances

--- lab4/backProjection_reconstruction.m
+++ lab4/backProjection_reconstruction.m
@@ -22,14 +22,18 @@ function G = backProjection_reconstruction(data, resolution_voxel, resolution_ca
         for l_j = 1:size(x_l, 2)
             l = x_l(l_i, l_j, :);
             l = l(:);
+            % Distance from the laser to the rellay wall
             d1 = norm(l - laser_origin(:));
             for s_i = 1:resolution_capture:size(x_s, 1)
                 for s_j = 1:resolution_capture:size(x_s, 2)
                     s = x_s(s_i, s_j, :);
                     s = s(:);
-                    d2 = norm(spad_origin(:) - s);
-                    d3 = vecnorm(bsxfun(@minus, center, l), 2, 1);
-                    d4 = vecnorm(bsxfun(@minus, center, s), 2, 1);
+                    % Distance from the relay wall to the SPAD
+                    d4 = norm(spad_origin(:) - s);
+                    % Distance from the voxel to the relay wall laser
+                    d2 = vecnorm(bsxfun(@minus, center, l), 2, 1);
+                    % Distance from the voxel to the relay wall SPAD
+                    d3 = vecnorm(bsxfun(@minus, center, s), 2, 1);
                     t = (d1 + d2 + d3 + d4);
                     index = round(t / data.deltaT) + data.t0;
                     G = G + reshape(data.data(l_i, l_j, s_i, s_j, index), size(G));
