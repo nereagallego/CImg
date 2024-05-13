ccbdd99aefb81939118409b2b6dfb06c7ab3356d
Nerea <801950@unizar.es>
2024-05-14 00:09:43 +0200

    confocal

---
+++ lab4/backProjection_confocal_reconstruction.m
@@ -0,0 +1,41 @@
+function G = backProjection_confocal_reconstruction(data, resolution_voxel, resolution_capture)
+    laser_origin = data.laserOrigin;
+    spad_origin = data.spadOrigin;
+
+    x_l = data.laserPositions;
+    x_s = data.spadPositions;
+
+    v_c = data.volumePosition;
+    v_s = data.volumeSize;
+
+    % Create a 3D grid of voxel indices
+    [v_i, v_j, v_k] = ndgrid(1:resolution_voxel, 1:resolution_voxel, 1:resolution_voxel);
+
+    % Calculate the center for each voxel
+    center = v_c - 0.5 * [v_s; v_s; v_s] + [v_i(:)'; v_j(:)'; v_k(:)'] * v_s / resolution_voxel;
+
+    % Initialize G
+    G = zeros(resolution_voxel, resolution_voxel, resolution_voxel);
+
+    % Loop over l and s
+    for l_i = 1:size(x_l, 1)
+        for l_j = 1:size(x_l, 2)
+            l = x_l(l_i, l_j, :);
+            l = l(:);
+            d1 = norm(l - laser_origin(:));
+            
+            s = x_s(l_i, l_j, :);
+            s = s(:);
+            d2 = norm(spad_origin(:) - s);
+            d3 = vecnorm(bsxfun(@minus, center, l), 2, 1);
+            d4 = vecnorm(bsxfun(@minus, center, s), 2, 1);
+            t = (d1 + d2 + d3 + d4);
+            index = round(t / data.deltaT) + data.t0;
+            G = G + reshape(data.data(l_i, l_j, index), size(G));
+            
+        end
+    end
+
+    
+    
+end
