edbd4897a9014e9231803c665197ec1ed7f1e7dc
cborjamoreno <cesarborjamoreno@gmail.com>
2024-05-15 20:45:39 +0200

    concavities try

--- lab4/lab4.m
+++ lab4/lab4.m
@@ -1,14 +1,14 @@
 % NLOS Imaging
 
 % Load the data
-% data = load_hdf5_dataset('Data\Z_d=0.5_l=[1x1]_s=[256x256].hdf5');
+data = load_hdf5_dataset('data/concavities_l[0.00,-0.50,0.00]_r[1.57,0.00,3.14]_v[0.81,0.51,0.80]_s[256]_l[256]_gs[1.00]_conf.hdf5');
 
 % Start the timer
 
 % tic;
 % 
 % % Perform the back-projection reconstruction
-% G = backProjection_reconstruction(data, 16, 1);
+G = backProjection_confocal_reconstruction(data, 16, 1);
 % 
 % % Stop the timer and display the elapsed time
 % elapsed_time = toc;
@@ -19,12 +19,12 @@
 % filename = sprintf('Z_d=0.5_l=[1x1]_s=[256x256]_v=%d_c=%d.mat', 16, 1);
 % save(filename, 'G');
 
-G = load("bunny_d=0.5_c=[256x256]_v=16_c=1.mat");
-G = G.G;
+%G = load("bunny_d=0.5_c=[256x256]_v=16_c=1.mat");
+%G = G.G;
 
 f_lap = fspecial3('lap');
 G_lap = imfilter(G, -f_lap, "symmetric");
-% volumeViewer(G);
+%volumeViewer(G);
 volumeViewer(G_lap)
 G_squeeze = squeeze(max(G, [], 3));
 colormap(hot(256));
